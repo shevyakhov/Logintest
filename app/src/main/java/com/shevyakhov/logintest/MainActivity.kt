1235bc21fde0397322b4183cc191e0f70a422c98
Artem <shevyakhov89@gmail.com>
2023-11-16 21:47:28 +0700

    base/ multi-module foundation

--- app/src/main/java/com/shevyakhov/logintest/MainActivity.kt
+++ app/src/main/java/com/shevyakhov/logintest/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.shevyakhov.logintest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.terrakok.cicerone.NavigatorHolder
+import com.github.terrakok.cicerone.androidx.AppNavigator
+
+import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.qualifier.named
 
 class MainActivity : AppCompatActivity() {
+
+
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+	}
+
+	override fun onDestroy() {
+		super.onDestroy()
+
+
 	}
 }
