22d8a9b3f69a2f07e6b0dd03393d5ac9c3aaa5ac
Artem <shevyakhov89@gmail.com>
2023-11-17 12:05:57 +0700

    feature/ Network Module

--- app/src/main/java/com/shevyakhov/logintest/App.kt
+++ app/src/main/java/com/shevyakhov/logintest/App.kt
@@ -2,6 +2,8 @@ package com.shevyakhov.logintest
 
 import android.app.Application
 import com.shevyakhov.features.splash.di.SplashModule
+import com.shevyakhov.libraries.network.di.BACKEND
+import com.shevyakhov.libraries.network.di.NetworkModule
 import com.shevyakhov.logintest.di.AppModule
 import com.shevyakhov.logintest.di.RouterModule
 import org.koin.android.ext.koin.androidContext
@@ -12,11 +14,16 @@ class App : Application() {
 	override fun onCreate() {
 		super.onCreate()
 		startKoin {
+			properties(
+				mapOf(BACKEND to BuildConfig.BASE_URL),
+			)
+
 			androidContext(this@App)
 			modules(AppModule)
 			modules(RouterModule)
 
 			modules(SplashModule)
+			modules(NetworkModule)
 		}
 	}
 }
