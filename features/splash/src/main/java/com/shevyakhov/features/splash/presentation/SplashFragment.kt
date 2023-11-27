ace917512496beb760ff15fe3b0cde3d6423f98c
Artem <shevyakhov89@gmail.com>
2023-11-27 16:54:29 +0700

    feature/login navigation if logged

--- features/splash/src/main/java/com/shevyakhov/features/splash/presentation/SplashFragment.kt
+++ features/splash/src/main/java/com/shevyakhov/features/splash/presentation/SplashFragment.kt
@@ -1,10 +1,12 @@
 package com.shevyakhov.features.splash.presentation
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.shevyakhov.feature.splash.R
 import com.shevyakhov.feature.splash.databinding.FragmentSplashBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -24,7 +26,10 @@ class SplashFragment : Fragment() {
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
-		viewModel.navigateToHome()
+
+		val sharedPreferences = requireContext().getSharedPreferences(getString(R.string.logged_shared_pref), Context.MODE_PRIVATE)
+		val isLogged = sharedPreferences.getBoolean(getString(R.string.is_logged), false)
+		viewModel.handleNavigation(isLogged)
 	}
 
 	override fun onDestroyView() {
